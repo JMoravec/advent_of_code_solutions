e25d8f81ba4fc9c15cdcb8b84406e21fce8bcb18
Joshua Moravec <me@jmoravec.com>
2020-12-02 18:44:28 -0600

    Add Day2-part2 solution

--- 2020/day2/day2.py
+++ 2020/day2/day2.py
@@ -24,7 +24,7 @@ def parse_password_rule(input_str: str) -> PasswordRule:
     min_value, max_value = range_str.split('-')
     return PasswordRule(int(min_value),int(max_value),letter)
 
-def apply_rule(password: str, rule: PasswordRule) -> bool:
+def apply_rule_part_1(password: str, rule: PasswordRule) -> bool:
     """
     Apply a rule to a password to check if it is acceptable
     """
@@ -38,18 +38,41 @@ def apply_rule(password: str, rule: PasswordRule) -> bool:
 
     return current_letters >= rule.min_value
 
+
+def apply_rule_part_2(password: str, rule: PasswordRule) -> bool:
+    """
+    Apply a rule to a password to check if it is acceptable
+    """
+    first_pos = password[rule.min_value-1] == rule.character
+    second_pos = password[rule.max_value-1] == rule.character
+
+    if first_pos and second_pos:
+        return False
+    if not first_pos and not second_pos:
+        return False
+
+    return True
+
+
 def main():
+    """
+    Main method for the problem set
+    """
     with open('day2_input.txt') as f:
         all_inputs = f.readlines()
 
-    total_valid_passwords = 0
+    total_valid_passwords_part1 = 0
+    total_valid_passwords_part2 = 0
     for input_str in all_inputs:
         rule_str, password = input_str.split(': ')
         rule = parse_password_rule(rule_str)
-        if apply_rule(password, rule):
-            total_valid_passwords += 1
+        if apply_rule_part_1(password, rule):
+            total_valid_passwords_part1 += 1
+        if apply_rule_part_2(password, rule):
+            total_valid_passwords_part2 += 1
 
-    print(total_valid_passwords)
+    print(f'Part 1: {total_valid_passwords_part1}')
+    print(f'Part 2: {total_valid_passwords_part2}')
 
 if __name__ == '__main__':
     main()
--- 2020/day2/day2_test.py
+++ 2020/day2/day2_test.py
@@ -2,7 +2,8 @@
 Test module for day 2 2020
 """
 import pytest
-from day2.day2 import PasswordRule, parse_password_rule, apply_rule
+from day2.day2 import PasswordRule, parse_password_rule, \
+    apply_rule_part_1, apply_rule_part_2
 
 
 @pytest.mark.parametrize('input_str,expected',
@@ -20,8 +21,18 @@ def test_parse_password_rule(input_str: str, expected: PasswordRule):
     [('abcde', PasswordRule(1,3,'a'),True),
     ('cdefg', PasswordRule(1,3,'b'),False),
     ('ccccccccc', PasswordRule(2,9, 'c'),True)])
-def test_apply_rule(password: str, rule: PasswordRule, expected: bool):
+def test_apply_rule_part1(password: str, rule: PasswordRule, expected: bool):
     """
     Validate that the given rule will correctly check a given string
     """
-    assert apply_rule(password, rule) == expected
+    assert apply_rule_part_1(password, rule) == expected
+
+@pytest.mark.parametrize('password,rule,expected',
+    [('abcde', PasswordRule(1,3,'a'),True),
+    ('cdefg', PasswordRule(1,3,'b'),False),
+    ('ccccccccc', PasswordRule(2,9, 'c'),False)])
+def test_apply_rule_part2(password: str, rule: PasswordRule, expected: bool):
+    """
+    Validate that the given rule will correctly check a given string
+    """
+    assert apply_rule_part_2(password, rule) == expected
