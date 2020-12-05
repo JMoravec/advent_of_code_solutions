433147bccfee64aafc3b3e7593e5eb57fd3dbf52
Joshua Moravec <me@jmoravec.com>
2020-12-05 00:21:23 -0600

    Add Day4-part2 solution

--- 2020/day4/day4.py
+++ 2020/day4/day4.py
@@ -4,14 +4,123 @@ Day 4 of advent of code 2020
 from typing import List
 
 
-def is_passport_valid(input_str: str) -> bool:
+def is_passport_valid(
+    input_str: str, validate_fields: bool, debug: bool = False
+) -> bool:
     """
     Check if a passport is valid
     """
+    valid = True
     strings_to_check = ["byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"]
     for test_string in strings_to_check:
         if test_string not in input_str:
+            valid = False
+            break
+
+    if not valid or not validate_fields:
+        return valid
+
+    input_str = input_str.replace("\n", " ")
+    fields = input_str.split(" ")
+    for field in fields:
+        if not field:
+            continue
+        if not validate_field(field):
+            if debug:
+                print(f"Field: {field}")
             return False
+
+    return True
+
+
+def validate_byr(value: str) -> bool:
+    """
+    Validate the byr field
+    """
+    return int(value) >= 1920 and int(value) <= 2002
+
+
+def validate_iyr(value: str) -> bool:
+    """
+    Validate the iyr field
+    """
+    return int(value) >= 2010 and int(value) <= 2020
+
+
+def validate_eyr(value: str) -> bool:
+    """
+    Validate the eyr field
+    """
+    return int(value) >= 2020 and int(value) <= 2030
+
+
+def validate_hgt(value: str) -> bool:
+    """
+    Validate the hgt field
+    """
+    if "cm" in value:
+        return int(value[0:-2]) >= 150 and int(value[0:-2]) <= 193
+    if "in" in value:
+        return int(value[0:-2]) >= 59 and int(value[0:-2]) <= 76
+    return False
+
+
+def validate_hcl(value: str) -> bool:
+    """
+    Validate the hcl field
+    """
+    if len(value) != 7 or value[0] != "#":
+        return False
+
+    try:
+        int(value[1:], 16)
+        return True
+    except ValueError:
+        return False
+
+
+def validate_ecl(value: str) -> bool:
+    """
+    Validate the ecl field
+    """
+    return value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+
+
+def validate_pid(value: str) -> bool:
+    """
+    Validate the pid field
+    """
+    if len(value) != 9:
+        return False
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+def validate_field(field: str) -> bool:
+    """
+    Validate a field
+    """
+    field_split = field.split(":")
+    key = field_split[0]
+    value = field_split[1]
+
+    if key == "byr":
+        return validate_byr(value)
+    if key == "iyr":
+        return validate_iyr(value)
+    if key == "eyr":
+        return validate_eyr(value)
+    if key == "hgt":
+        return validate_hgt(value)
+    if key == "hcl":
+        return validate_hcl(value)
+    if key == "ecl":
+        return validate_ecl(value)
+    if key == "pid":
+        return validate_pid(value)
     return True
 
 
@@ -40,7 +149,19 @@ def get_day1_answer(input_str: str) -> int:
     parsed_input = parse_full_input(input_str.split("\n"))
     total = 0
     for passport in parsed_input:
-        if is_passport_valid(passport):
+        if is_passport_valid(passport, False):
+            total += 1
+    return total
+
+
+def get_day2_answer(input_str: str) -> int:
+    """
+    Get the day 2 answer from an input string
+    """
+    parsed_input = parse_full_input(input_str.split("\n"))
+    total = 0
+    for passport in parsed_input:
+        if is_passport_valid(passport, True):
             total += 1
     return total
 
@@ -50,9 +171,10 @@ def main():
     Main method to run the day's input
     """
     full_input: str
-    with open("day4_input.txt") as f:
-        full_input = f.read()
+    with open("day4_input.txt") as input_file:
+        full_input = input_file.read()
     print(f"Day 1: {get_day1_answer(full_input)}")
+    print(f"Day 2: {get_day2_answer(full_input)}")
 
 
 if __name__ == "__main__":
--- 2020/day4/day4_test.py
+++ 2020/day4/day4_test.py
@@ -3,32 +3,130 @@ Test module for day 4 2020
 """
 from typing import List
 import pytest
-from day4.day4 import is_passport_valid, parse_full_input, get_day1_answer
+from day4.day4 import (
+    is_passport_valid,
+    parse_full_input,
+    get_day1_answer,
+    validate_byr,
+    validate_hgt,
+    validate_hcl,
+    validate_ecl,
+    validate_pid,
+)
+
+
+@pytest.mark.parametrize("test,expected", [("2002", True), ("2003", False)])
+def test_validate_byr(test: str, expected: bool):
+    assert validate_byr(test) == expected
 
 
 @pytest.mark.parametrize(
-    "test_input,expected",
+    "test,expected", [("60in", True), ("190cm", True), ("190in", False), ("190", False)]
+)
+def test_validate_hgt(test: str, expected: bool):
+    assert validate_hgt(test) == expected
+
+
+@pytest.mark.parametrize(
+    "test,expected",
+    [
+        ("#123abc", True),
+        ("#123abz", False),
+        ("123abc", False),
+    ],
+)
+def test_validate_hcl(test: str, expected: bool):
+    assert validate_hcl(test) == expected
+
+
+@pytest.mark.parametrize(
+    "test,expected",
+    [
+        ("brn", True),
+        ("wat", False),
+    ],
+)
+def test_validate_ecl(test: str, expected: bool):
+    assert validate_ecl(test) == expected
+
+
+@pytest.mark.parametrize(
+    "test,expected",
+    [
+        ("000000001", True),
+        ("0123456789", False),
+    ],
+)
+def test_validate_pid(test: str, expected: bool):
+    assert validate_pid(test) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input,validate_fields,expected",
     [
         (
             "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm",
+            False,
             True,
         ),
         (
             "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929",
             False,
+            False,
         ),
         (
             "hcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm",
+            False,
             True,
         ),
-        ("hcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in", False),
+        ("hcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in", False, False),
+        (
+            "eyr:1972 cid:100\nhcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+            True,
+            False,
+        ),
+        (
+            "iyr:2019\nhcl:#602927 eyr:1967 hgt:170cm\necl:grn pid:012533040 byr:1946",
+            True,
+            False,
+        ),
+        (
+            "hcl:dab227 iyr:2012\necl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+            True,
+            False,
+        ),
+        (
+            "hgt:59cm ecl:zzz\neyr:2038 hcl:74454a iyr:2023\npid:3556412378 byr:2007",
+            True,
+            False,
+        ),
+        (
+            "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\n hcl:#623a2f",
+            True,
+            True,
+        ),
+        (
+            "eyr:2029 ecl:blu cid:129 byr:1989\niyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+            True,
+            True,
+        ),
+        (
+            "hcl:#888785\nhgt:164cm byr:2001 iyr:2015 cid:88\npid:545766238 ecl:hzl\neyr:2022",
+            True,
+            True,
+        ),
+        (
+            "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+            True,
+            True,
+        ),
     ],
 )
-def test_validate_passport(test_input: str, expected: bool):
+def test_validate_passport(test_input: str, validate_fields: bool, expected: bool):
     """
     Validate that the code can determine if a passport is valid
     """
-    assert is_passport_valid(test_input) == expected
+    assert is_passport_valid(test_input, validate_fields) == expected
 
 
 @pytest.mark.parametrize(
