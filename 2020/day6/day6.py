c668f8a572e7c2fbc139e9bc1f98d3a022f138ba
Joshua Moravec <me@jmoravec.com>
2020-12-06 20:44:32 -0600

    Add Day6-part2 solution

--- 2020/day6/day6.py
+++ 2020/day6/day6.py
@@ -1,6 +1,8 @@
 """
 Day 6 of advent of code 2020
 """
+from typing import Dict
+import itertools
 
 
 def count_group(input_str: str) -> int:
@@ -15,6 +17,30 @@ def count_group(input_str: str) -> int:
     return len(unique_choices)
 
 
+def count_all_group(input_str: str) -> int:
+    """
+    Count the amount of answers that are common to each person in a group
+    """
+    individuals = input_str.strip().split("\n")
+    unique_choices: Dict[str, int] = {}
+    for char in individuals[0]:
+        unique_choices[char] = 1
+
+    if len(individuals) == 1:
+        return len(unique_choices)
+
+    for individual in individuals[1:]:
+        for char in individual:
+            if char in unique_choices.keys():
+                unique_choices[char] += 1
+
+    total = 0
+    for value in unique_choices.values():
+        if value == len(individuals):
+            total += 1
+    return total
+
+
 def solve_part_1(input_str: str) -> int:
     """
     Solve part 1 for a given input
@@ -26,6 +52,17 @@ def solve_part_1(input_str: str) -> int:
     return total
 
 
+def solve_part_2(input_str: str) -> int:
+    """
+    Solve part 2 for a given input
+    """
+    all_groups = input_str.split("\n\n")
+    total = 0
+    for group in all_groups:
+        total += count_all_group(group)
+    return total
+
+
 def main():
     """
     Main method to run the day's input
@@ -33,7 +70,7 @@ def main():
     with open("day6_input.txt") as problem_file:
         all_inputs = problem_file.read()
     print(f"Part 1: {solve_part_1(all_inputs)}")
-    # print(f"Part 2: {solve_part_2(all_inputs)}")
+    print(f"Part 2: {solve_part_2(all_inputs)}")
 
 
 if __name__ == "__main__":
--- 2020/day6/day6_test.py
+++ 2020/day6/day6_test.py
@@ -2,7 +2,7 @@
 Test module for day 6 2020
 """
 import pytest
-from day6.day6 import solve_part_1, count_group
+from day6.day6 import solve_part_1, count_group, count_all_group, solve_part_2
 
 
 @pytest.mark.parametrize(
@@ -17,6 +17,17 @@ def test_count_group(input_str: str, expected: int):
 
 
 @pytest.mark.parametrize(
+    "input_str,expected",
+    [("abc\n", 3), ("a\nb\nc\n", 0), ("ab\nac\n", 1), ("a\na\na\na\n", 1), ("b\n", 1)],
+)
+def test_count_all_group(input_str: str, expected: int):
+    """
+    Validate that a single group is counted correctly
+    """
+    assert count_all_group(input_str) == expected
+
+
+@pytest.mark.parametrize(
     "input_str,expected", [("abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb", 11)]
 )
 def test_solve_part_1(input_str: str, expected: int):
@@ -24,3 +35,13 @@ def test_solve_part_1(input_str: str, expected: int):
     Validate that the exmple is correctly solved for part 1
     """
     assert solve_part_1(input_str) == expected
+
+
+@pytest.mark.parametrize(
+    "input_str,expected", [("abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb", 6)]
+)
+def test_solve_part_2(input_str: str, expected: int):
+    """
+    Validate that the exmple is correctly solved for part 2
+    """
+    assert solve_part_2(input_str) == expected
