ffba5358be263d083b359477b096cd59ab7eceed
Joshua Moravec <me@jmoravec.com>
2020-12-10 09:27:38 -0600

    Add Day9-part2 solution

--- 2020/day9/day9.py
+++ 2020/day9/day9.py
@@ -31,6 +31,36 @@ def solve_part_1(preamble_length: int, inputs: List[int]) -> int:
     return -1
 
 
+class DefinitlyNotAnswerException(Exception):
+    """ Helper exception """
+
+
+def _check_inner_loop(inputs: List[int], start_index: int, part_1_answer: int) -> int:
+    for end_delta in range(1, len(inputs[start_index:])):
+        test_answer = sum(inputs[start_index : start_index + end_delta])
+        if test_answer == part_1_answer:
+            return min(inputs[start_index : start_index + end_delta]) + max(
+                inputs[start_index : start_index + end_delta]
+            )
+        if test_answer > part_1_answer:
+            raise DefinitlyNotAnswerException()
+    return -1
+
+
+def solve_part_2(part_1_answer: int, inputs: List[int]) -> int:
+    """
+    Solve part 1 of day 9
+    """
+    for start_index, _ in enumerate(inputs):
+        try:
+            answer = _check_inner_loop(inputs, start_index, part_1_answer)
+            if answer != -1:
+                return answer
+        except DefinitlyNotAnswerException:
+            pass
+    return -1
+
+
 def main():
     """
     Main method to run the day's input
@@ -39,8 +69,9 @@ def main():
         all_inputs = problem_file.readlines()
 
     all_inputs_int = [int(number) for number in all_inputs]
-    print(f"Part 1: {solve_part_1(25, all_inputs_int)}")
-    # print(f"Part 2: {solve_part_2(all_inputs)}")
+    part_1_answer = solve_part_1(25, all_inputs_int)
+    print(f"Part 1: {part_1_answer}")
+    print(f"Part 2: {solve_part_2(part_1_answer, all_inputs_int)}")
 
 
 if __name__ == "__main__":
--- 2020/day9/day9_test.py
+++ 2020/day9/day9_test.py
@@ -3,7 +3,7 @@ Test module for day 8 2020
 """
 from typing import List
 import pytest
-from day9.day9 import solve_part_1
+from day9.day9 import solve_part_1, solve_part_2
 
 
 @pytest.mark.parametrize(
@@ -39,3 +39,38 @@ from day9.day9 import solve_part_1
 )
 def test_solve_part_1(preamble_length: int, inputs: List[int], expected: int):
     assert solve_part_1(preamble_length, inputs) == expected
+
+
+@pytest.mark.parametrize(
+    "preamble_length,inputs,expected",
+    [
+        (
+            5,
+            [
+                35,
+                20,
+                15,
+                25,
+                47,
+                40,
+                62,
+                55,
+                65,
+                95,
+                102,
+                117,
+                150,
+                182,
+                127,
+                219,
+                299,
+                277,
+                309,
+                576,
+            ],
+            62,
+        )
+    ],
+)
+def test_solve_part_2(preamble_length: int, inputs: List[int], expected: int):
+    assert solve_part_2(127, inputs) == expected
