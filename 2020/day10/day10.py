9663d799d484ea568ce49ebe7549bf8186ea7bce
Joshua Moravec <me@jmoravec.com>
2020-12-11 09:14:37 -0600

    Add Day10-part2 solution

--- 2020/day10/day10.py
+++ 2020/day10/day10.py
@@ -1,7 +1,7 @@
 """
 Day 10 of advent of code 2020
 """
-from typing import List
+from typing import List, Dict
 
 
 def solve_part_1(input_nums: List[int]) -> int:
@@ -32,6 +32,23 @@ def solve_part_1(input_nums: List[int]) -> int:
     return one_jumps * three_jumps
 
 
+def solve_part_2(input_nums: List[int]) -> int:
+    """
+    Solve part 2 of the day
+    """
+    input_nums.sort()
+    input_nums.insert(0, 0)
+    paths_to_end: Dict[int, int] = {max(input_nums): 1}
+    for value in reversed(input_nums[:-1]):
+        total_paths_for_value = 0
+        total_paths_for_value += paths_to_end.get(value + 1, 0)
+        total_paths_for_value += paths_to_end.get(value + 2, 0)
+        total_paths_for_value += paths_to_end.get(value + 3, 0)
+        paths_to_end[value] = total_paths_for_value
+
+    return paths_to_end[0]
+
+
 def main():
     """
     Main method to run the day's input
@@ -42,7 +59,7 @@ def main():
     all_inputs_int = [int(number) for number in all_inputs]
     part_1_answer = solve_part_1(all_inputs_int)
     print(f"Part 1: {part_1_answer}")
-    # print(f"Part 2: {solve_part_2(part_1_answer, all_inputs_int)}")
+    print(f"Part 2: {solve_part_2(all_inputs_int)}")
 
 
 if __name__ == "__main__":
--- 2020/day10/day10_test.py
+++ 2020/day10/day10_test.py
@@ -3,7 +3,7 @@ Test module for day 10 2020
 """
 from typing import List
 import pytest
-from day10.day10 import solve_part_1
+from day10.day10 import solve_part_1, solve_part_2
 
 
 @pytest.mark.parametrize(
@@ -68,3 +68,67 @@ def test_solve_part_1(input_values: List[int], expected: int):
     Validate part 1 of the day
     """
     assert solve_part_1(input_values) == expected
+
+
+@pytest.mark.parametrize(
+    "input_values,expected",
+    [
+        (
+            [
+                16,
+                10,
+                15,
+                5,
+                1,
+                11,
+                7,
+                19,
+                6,
+                12,
+                4,
+            ],
+            8,
+        ),
+        (
+            [
+                28,
+                33,
+                18,
+                42,
+                31,
+                14,
+                46,
+                20,
+                48,
+                47,
+                24,
+                23,
+                49,
+                45,
+                19,
+                38,
+                39,
+                11,
+                1,
+                32,
+                25,
+                35,
+                8,
+                17,
+                7,
+                9,
+                4,
+                2,
+                34,
+                10,
+                3,
+            ],
+            19208,
+        ),
+    ],
+)
+def test_solve_part_2(input_values: List[int], expected: int):
+    """
+    Validate part 1 of the day
+    """
+    assert solve_part_2(input_values) == expected
